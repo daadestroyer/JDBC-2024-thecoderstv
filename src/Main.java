9c575d691407884f40833e658e9300ad29b50c8e
Shubham Nigam <48306820+daadestroyer@users.noreply.github.com>
2024-05-25 12:04:47 +0530

    connection program...

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Main {
+
+    // database information
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/jdbc";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "root1998";
+
+    private static Connection connection;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+            if (connection != null) {
+                System.out.println("Connection done...");
+            } else {
+                System.out.println("Connection fail");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
