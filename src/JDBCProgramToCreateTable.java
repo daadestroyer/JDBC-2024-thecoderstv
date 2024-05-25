9e9d62911406e7faa152f0c70c95600e17193494
Shubham Nigam <48306820+daadestroyer@users.noreply.github.com>
2024-05-25 12:39:50 +0530

    jdbc program to create table...

---
+++ src/JDBCProgramToCreateTable.java
@@ -0,0 +1,32 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class JDBCProgramToCreateTable {
+
+    // database information
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/jdbc";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "root1998";
+
+    // use to make connection from db
+    private static Connection connection;
+
+    // use to fire query(select * from student where id = 101)
+    private static Statement statement;
+    public static void main(String[] args) {
+        try{
+            connection = DriverManager.getConnection(DB_URL,DB_USERNAME,DB_PASSWORD);
+            statement = connection.createStatement();
+            String sql = "CREATE TABLE IF NOT EXISTS STUDENT(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(200) , city VARCHAR(50), email VARCHAR(50))";
+
+            // fire query
+            statement.executeUpdate(sql);
+            System.out.println("Table created...");
+        }
+        catch (Exception e){
+            System.out.println("Table creation failed...");
+            e.printStackTrace();
+        }
+    }
+}
